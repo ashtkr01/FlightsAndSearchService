fc3c17bf1dac399d68b97b8dd1124185ed1d8680
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2023-02-05 09:50:56 +0530

    Implement CRUD operation to Airport using common CRUD Classes

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -21,92 +21,93 @@ const create = async(req , res) => {
         });
     }
 }
-// const { AirportService } = require('../services/index');
 
-// const airportService = new AirportService();
+const destroy = async (req , res) => {
+    try {
+        const airport = await airportService.destroy(req.params.id);
+        return res.status(200).json({
+           data: airport,
+           success: true,
+           message: "Successfully delete an Airport",
+           error: {} 
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: "",
+            success: false,
+            message: "Not able to delete an Airport",
+            error: error
+        });
+    }
+}
 
-// const create = async (req , res) => {
-//     try {
-//         const airport = await airportService.createAirport(req.body);
-//         return res.status(201).json({
-//            data: airport,
-//            success: true,
-//            message: "Successfully Create an Airport",
-//            error: {} 
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//             data: {},
-//             success: false,
-//             message: "Not able to create an Airport",
-//             error: error
-//         });
-//     }
-// }
+const update = async (req , res) => {
+    try {
+        const airport = await airportService.update(req.params.id , req.body);
+        return res.status(200).json({
+           data: airport,
+           success: true,
+           message: "Successfully updates an Airport",
+           error: {} 
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to update an Airport",
+            error: error
+        });
+    }
+}
 
-// const destroy = async (req , res) => {
-//     try {
-//         const airport = await airportService.deleteAirport(req.params.id);
-//         return res.status(200).json({
-//            data: airport,
-//            success: true,
-//            message: "Successfully delete an Airport",
-//            error: {} 
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//             data: {},
-//             success: false,
-//             message: "Not able to delete an Airport",
-//             error: error
-//         });
-//     }
-// }
-// const update = async (req , res) => {
-//     try {
-//         const airport = await airportService.updateAirport(req.params.id , req.body);
-//         return res.status(200).json({
-//            data: airport,
-//            success: true,
-//            message: "Successfully delete an Airport",
-//            error: {} 
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//             data: {},
-//             success: false,
-//             message: "Not able to delete an Airport",
-//             error: error
-//         });
-//     }
-// }
+const get = async (req , res) => {
+    try {
+        const airport = await airportService.get(req.params.id);
+        return res.status(200).json({
+           data: airport,
+           success: true,
+           message: "Successfully got an Airport",
+           error: {} 
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to get an Airport",
+            error: error
+        });
+    }
+}
 
-// const get = async (req , res) => {
-//     try {
-//         const airport = await airportService.getAirport(req.params.id);
-//         return res.status(200).json({
-//            data: airport,
-//            success: true,
-//            message: "Successfully delete an Airport",
-//            error: {} 
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//             data: {},
-//             success: false,
-//             message: "Not able to delete an Airport",
-//             error: error
-//         });
-//     }
-// }
+
+const getAll = async (req , res) => {
+    try {
+        const airports = await airportService.getAll();
+        return res.status(200).json({
+            data: airports,
+            success: true,
+            message: "Successfully got all the airports",
+            error: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to get all Airports",
+            error: error
+        });
+    }
+}
+
 
 module.exports = {
-    create
-    // destroy,
-    // update,
-    // get
+    create,
+    destroy,
+    update,
+    get,
+    getAll
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,18 +6,19 @@ const FlightController = require('../../controllers/flight-controller');
 const { FlightMiddlewares } = require('../../middlewares/index');
 
 const router = express.Router();
-
+//City:
 router.post('/city',CityController.create);
 router.delete('/city/:id',CityController.destroy);
 router.get('/city/:id',CityController.get);
 router.get('/city',CityController.getAll);
 router.patch('/city/:id',CityController.update);
 router.post('/cityBulk',CityController.createAll);
-
-router.post('/airports',AirportController.create);
-// router.delete('/airport/:id',AirportController.destroy);
-// router.get('/airport/:id',AirportController.get);
-// router.patch('/airport/:id',AirportController.update);
+//Airport:
+router.post('/airport',AirportController.create);
+router.delete('/airport/:id',AirportController.destroy);
+router.get('/airport/:id',AirportController.get);
+router.patch('/airport/:id',AirportController.update);
+router.get('/airports' , AirportController.getAll);
 
 //Flight:
 router.get('/flights',FlightController.getAll);
